d5d966a1ef33755c5eb3c1879e59edd8a458a617
Simon Cheng <simon.cheng@shopify.com>
2017-07-22 19:07:38 -0400

    Table view

--- bittrex.js
+++ bittrex.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     $( "#reloadBittrex" ).click(function() {
       reloadBittrex();
     });
@@ -8,7 +7,35 @@ $( document ).ready(function() {
 function reloadBittrex() {
   $.get( "https://bittrex.com/api/v1.1/public/getmarketsummaries",
     function( data ) {
-      console.log(data);
+
+      var data = data.result
+      console.log(data)
+      var body = $('#bittrexTable'),
+
+      props = [
+        "Ask",
+        "BaseVolume",
+        "Bid",
+        "Created",
+        "High",
+        "Last",
+        "Low",
+        "MarketName",
+        "OpenBuyOrders",
+        "OpenSellOrders",
+        "PrevDay",
+        "TimeStamp",
+        "Volume"
+      ];
+
+      $.each(data, function(i, data) {
+        var tr = $('<tr>');
+        $.each(props, function(i, prop) {
+          $('<td>').html(data[prop]).appendTo(tr);
+        });
+        body.append(tr);
+      });
+
     }
   );
 }
